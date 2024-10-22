77fc995c1591b752af2daa19786dcc6af927cf4e
tetraxile <tetraxile@proton.me>
2024-10-22 03:11:44 +0100

    add byml reader

---
+++ include/byml.h
@@ -0,0 +1,76 @@
+#ifndef BYML_H
+#define BYML_H
+
+#include "util.h"
+
+class BYML {
+public:
+	enum class NodeType : u8 {
+		String = 0xa0,
+		Binary = 0xa1,
+		Array = 0xc0,
+		Hash = 0xc1,
+		StringTable = 0xc2,
+		Bool = 0xd0,
+		S32 = 0xd1,
+		F32 = 0xd2,
+		U32 = 0xd3,
+		S64 = 0xd4,
+		U64 = 0xd5,
+		F64 = 0xd6,
+		Null = 0xff,
+	};
+
+	struct Header {
+		util::ByteOrder mByteOrder;
+		u16 mVersion;
+		u32 mHashKeyTableOffset = 0;
+		u32 mStringValueTableOffset = 0;
+	};
+
+	enum Error : result_t {
+		WrongNodeType = 0x101,
+		InvalidKey = 0x102,
+		OutOfBounds = 0x103,
+	};
+
+	BYML();
+	result_t init(const u8* fileData);
+	result_t init(const u8* fileData, const u8* offset);
+
+	std::string get_hash_string(u32 idx) const;
+	std::string get_value_string(u32 idx) const;
+
+	NodeType get_type() const;
+	u32 get_size() const;
+	bool has_key(const std::string& key) const;
+
+	result_t get_container_by_idx(BYML* container, u32 idx) const;
+	result_t get_string_by_idx(std::string* out, u32 idx) const;
+	result_t get_bool_by_idx(bool* out, u32 idx) const;
+	result_t get_s32_by_idx(s32* out, u32 idx) const;
+	result_t get_f32_by_idx(f32* out, u32 idx) const;
+	result_t get_u32_by_idx(u32* out, u32 idx) const;
+	result_t get_s64_by_idx(s64* out, u32 idx) const;
+	result_t get_f64_by_idx(f64* out, u32 idx) const;
+	result_t get_u64_by_idx(u64* out, u32 idx) const;
+
+	result_t get_container_by_key(BYML* container, const std::string& key) const;
+	result_t get_string_by_key(std::string* out, const std::string& key) const;
+	result_t get_bool_by_key(bool* out, const std::string& key) const;
+	result_t get_s32_by_key(s32* out, const std::string& key) const;
+	result_t get_f32_by_key(f32* out, const std::string& key) const;
+	result_t get_u32_by_key(u32* out, const std::string& key) const;
+	result_t get_s64_by_key(s64* out, const std::string& key) const;
+	result_t get_f64_by_key(f64* out, const std::string& key) const;
+	result_t get_u64_by_key(u64* out, const std::string& key) const;
+
+private:
+	result_t get_node_by_key(const u8** offset, const std::string& key) const;
+
+	const u8* mFileData = nullptr;
+	const u8* mOffset = nullptr;
+	Header mHeader;
+};
+
+#endif
---
+++ src/byml.cpp
@@ -0,0 +1,402 @@
+#include "byml.h"
+
+#include <cstdio>
+
+BYML::BYML() {}
+
+result_t BYML::init(const u8* fileData) {
+	result_t r;
+
+	mFileData = fileData;
+
+	util::ByteOrder byteOrder;
+	r = reader::read_byte_order(&mHeader.mByteOrder, mFileData, 0x4259);
+	if (r) return r;
+
+	mHeader.mVersion = reader::read_u16(mFileData + 2, mHeader.mByteOrder);
+	mHeader.mHashKeyTableOffset = reader::read_u32(mFileData + 4, mHeader.mByteOrder);
+	mHeader.mStringValueTableOffset = reader::read_u32(mFileData + 8, mHeader.mByteOrder);
+	u32 rootOffset = reader::read_u32(mFileData + 0xc, mHeader.mByteOrder);
+
+	mOffset = mFileData + rootOffset;
+
+	return 0;
+}
+
+result_t BYML::init(const u8* fileData, const u8* offset) {
+	result_t r;
+
+	mFileData = fileData;
+	mOffset = offset;
+
+	util::ByteOrder byteOrder;
+	r = reader::read_byte_order(&mHeader.mByteOrder, mFileData, 0x4259);
+	if (r) return r;
+
+	mHeader.mVersion = reader::read_u16(mFileData + 2, mHeader.mByteOrder);
+	mHeader.mHashKeyTableOffset = reader::read_u32(mFileData + 4, mHeader.mByteOrder);
+	mHeader.mStringValueTableOffset = reader::read_u32(mFileData + 8, mHeader.mByteOrder);
+
+	return 0;
+}
+
+BYML::NodeType BYML::get_type() const {
+	return (NodeType)reader::read_u8(mOffset, mHeader.mByteOrder);
+}
+
+u32 BYML::get_size() const {
+	return reader::read_u24(mOffset + 1, mHeader.mByteOrder);
+}
+
+std::string BYML::get_hash_string(u32 idx) const {
+	const u8* base = mFileData + mHeader.mHashKeyTableOffset;
+
+	u32 size = reader::read_u24(base + 1, mHeader.mByteOrder);
+	if (idx >= size)
+		return "(null)";
+
+	u32 addr = reader::read_u32(base + 4 + (idx * 4), mHeader.mByteOrder);
+
+	const u8* strOffset = base + addr;
+	return reader::read_string(strOffset);
+}
+
+std::string BYML::get_value_string(u32 idx) const {
+	const u8* base = mFileData + mHeader.mStringValueTableOffset;
+
+	u32 size = reader::read_u24(base + 1, mHeader.mByteOrder);
+	if (idx >= size)
+		return "(null)";
+
+	u32 addr = reader::read_u32(base + 4 + (idx * 4), mHeader.mByteOrder);
+
+	const u8* strOffset = base + addr;
+	return reader::read_string(strOffset);
+}
+
+result_t BYML::get_node_by_key(const u8** offset, const std::string& key) const {
+	if (get_type() != NodeType::Hash)
+		return Error::WrongNodeType;
+
+	// TODO: implement binary search
+	for (u32 i = 0; i < get_size(); i++) {
+		const u8* childOffset = mOffset + 4 + i*8;
+		u32 keyIdx = reader::read_u24(childOffset, mHeader.mByteOrder);
+		if (util::is_equal(key, get_hash_string(keyIdx))) {
+			*offset = childOffset;
+			return 0;
+		}
+	}
+
+	return Error::InvalidKey;
+}
+
+bool BYML::has_key(const std::string& key) const {
+	if (get_type() != NodeType::Hash)
+		return false;
+
+	// TODO: implement binary search
+	for (u32 i = 0; i < get_size(); i++) {
+		const u8* childOffset = mOffset + 4 + i*8;
+		u32 keyIdx = reader::read_u24(childOffset, mHeader.mByteOrder);
+		if (util::is_equal(key, get_hash_string(keyIdx)))
+			return true;
+	}
+
+	return false;
+}
+
+result_t BYML::get_container_by_key(BYML* container, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::Array && type != NodeType::Hash)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	container->init(mFileData, (const u8*)(mFileData + value));
+	return 0;
+}
+
+result_t BYML::get_container_by_idx(BYML* container, u32 idx) const {
+	// TODO: allow indexing hash nodes by index?
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::Array && childType != NodeType::Hash)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	container->init(mFileData, (const u8*)(mFileData + value));
+	return 0;
+}
+
+result_t BYML::get_string_by_idx(std::string* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	*out = get_value_string(value);
+	return 0;
+}
+
+result_t BYML::get_bool_by_idx(bool* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	// note: treats non-boolean values (i.e. values that are not 0 or 1) as true
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	*out = value != 0;
+	return 0;
+}
+
+result_t BYML::get_s32_by_idx(s32* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	*out = reader::read_s32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_f32_by_idx(f32* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	*out = reader::read_f32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_u32_by_idx(u32* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	*out = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_s64_by_idx(s64* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	*out = reader::read_s64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_f64_by_idx(f64* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	*out = reader::read_f64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_u64_by_idx(u64* out, u32 idx) const {
+	if (get_type() != NodeType::Array)
+		return Error::WrongNodeType;
+
+	if (idx >= get_size())
+		return Error::OutOfBounds;
+
+	// round up to multiple of 4
+	u32 valuesOffset = (4 + get_size() + 3) & ~3;
+
+	NodeType childType = (NodeType)reader::read_u8(mOffset + 4 + idx, mHeader.mByteOrder);
+	if (childType != NodeType::String)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(mOffset + valuesOffset + 4*idx, mHeader.mByteOrder);
+	*out = reader::read_u64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_string_by_key(std::string* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::String)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	*out = get_value_string(value);
+	return 0;
+}
+
+result_t BYML::get_bool_by_key(bool* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::Bool)
+		return Error::WrongNodeType;
+	
+	// note: treats non-boolean values (i.e. values that are not 0 or 1) as true
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	*out = value != 0;
+	return 0;
+}
+
+
+result_t BYML::get_s32_by_key(s32* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::S32)
+		return Error::WrongNodeType;
+
+	*out = reader::read_s32(offset + 4, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_f32_by_key(f32* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::F32)
+		return Error::WrongNodeType;
+
+	*out = reader::read_f32(offset + 4, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_u32_by_key(u32* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::U32)
+		return Error::WrongNodeType;
+
+	*out = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_s64_by_key(s64* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::S64)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	*out = reader::read_s64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_f64_by_key(f64* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::F64)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	*out = reader::read_f64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
+
+result_t BYML::get_u64_by_key(u64* out, const std::string& key) const {
+	const u8* offset;
+	result_t r = get_node_by_key(&offset, key);
+	if (r) return r;
+
+	NodeType type = (NodeType)reader::read_u8(offset + 3, mHeader.mByteOrder);
+	if (type != NodeType::U64)
+		return Error::WrongNodeType;
+
+	u32 value = reader::read_u32(offset + 4, mHeader.mByteOrder);
+	*out = reader::read_u64(mFileData + value, mHeader.mByteOrder);
+	return 0;
+}
