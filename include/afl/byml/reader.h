e94b4403ee07cb285be26257ca63c3e96bec03a3
tetraxile <tetraxile@proton.me>
2025-04-21 22:55:21 +0100

    add `tryGet` functions in byml::Reader

--- include/afl/byml/reader.h
+++ include/afl/byml/reader.h
@@ -45,6 +45,26 @@ public:
 	result_t getF64ByKey(f64* out, const std::string& key) const;
 	result_t getU64ByKey(u64* out, const std::string& key) const;
 
+	bool tryGetContainerByIdx(Reader* container, u32 idx) const { return getContainerByIdx(container, idx) ? false : true; }
+	bool tryGetStringByIdx(std::string* out, u32 idx) const { return getStringByIdx(out, idx) ? false : true; }
+	bool tryGetBoolByIdx(bool* out, u32 idx) const { return getBoolByIdx(out, idx) ? false : true; }
+	bool tryGetS32ByIdx(s32* out, u32 idx) const { return getS32ByIdx(out, idx) ? false : true; }
+	bool tryGetF32ByIdx(f32* out, u32 idx) const { return getF32ByIdx(out, idx) ? false : true; }
+	bool tryGetU32ByIdx(u32* out, u32 idx) const { return getU32ByIdx(out, idx) ? false : true; }
+	bool tryGetS64ByIdx(s64* out, u32 idx) const { return getS64ByIdx(out, idx) ? false : true; }
+	bool tryGetF64ByIdx(f64* out, u32 idx) const { return getF64ByIdx(out, idx) ? false : true; }
+	bool tryGetU64ByIdx(u64* out, u32 idx) const { return getU64ByIdx(out, idx) ? false : true; }
+
+	bool tryGetContainerByKey(Reader* container, const std::string& key) const { return getContainerByKey(container, key) ? false : true; }
+	bool tryGetStringByKey(std::string* out, const std::string& key) const { return getStringByKey(out, key) ? false : true; }
+	bool tryGetBoolByKey(bool* out, const std::string& key) const { return getBoolByKey(out, key) ? false : true; }
+	bool tryGetS32ByKey(s32* out, const std::string& key) const { return getS32ByKey(out, key) ? false : true; }
+	bool tryGetF32ByKey(f32* out, const std::string& key) const { return getF32ByKey(out, key) ? false : true; }
+	bool tryGetU32ByKey(u32* out, const std::string& key) const { return getU32ByKey(out, key) ? false : true; }
+	bool tryGetS64ByKey(s64* out, const std::string& key) const { return getS64ByKey(out, key) ? false : true; }
+	bool tryGetF64ByKey(f64* out, const std::string& key) const { return getF64ByKey(out, key) ? false : true; }
+	bool tryGetU64ByKey(u64* out, const std::string& key) const { return getU64ByKey(out, key) ? false : true; }
+
 private:
 	struct Header {
 		util::ByteOrder mByteOrder;
